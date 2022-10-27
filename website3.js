f721f67dc0a0c292cba51306991b41a1fad99822
Alex <alexwusdbj@gmail.com>
2022-10-27 12:46:12 -0700

    Made format adjustable relative to window size

--- website3.js
+++ website3.js
@@ -1,14 +1,16 @@
 document.addEventListener("scroll", check);
 
+
+
 function show(id) {
   document.getElementById(id).classList.remove("hide");
   document.getElementById(id).style.display = 'block';
 }
 
 function check() {
-  console.log(document.documentElement.scrollTop);
-  if (document.documentElement.scrollTop > 600) {
-    show('hidden1');
-    show('hidden2');
+  var hideList = document.getElementsByClassName('hide');
+  for (e of hideList) {
+    console.log("Window"  + window.scrollY)
+    console.log(e.getBoundingClientRect().top - document.body.getBoundingClientRect().top);
   }
 }
